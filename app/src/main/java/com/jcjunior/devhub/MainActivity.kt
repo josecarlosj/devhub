a95019f68ac283ba9af092727d779c8bc4568f94
josecarlosj <junior.edf@gmail.com>
2023-07-09 19:09:40 -0300

    chore(day01): base card for developer's information

--- app/src/main/java/com/jcjunior/devhub/MainActivity.kt
+++ app/src/main/java/com/jcjunior/devhub/MainActivity.kt
@@ -3,13 +3,18 @@ package com.jcjunior.devhub
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jcjunior.devhub.ui.theme.DevHubTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +22,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DevHubTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    DeveloperInfo()
                 }
             }
         }
@@ -30,17 +34,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun DeveloperInfo() {
+    Column {
+        Image(modifier = Modifier.size(200.dp) , painter = painterResource(
+            id = R.drawable.avatar_sample),
+            contentDescription = null
+        )
+        Text(text = "Fulano da Silva")
+        Text(text = "fulanosilva-dev")
+        Text(text = "Desenvolvedor apaixonado por tecnologia e programação.")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DeveloperInfoPreview() {
     DevHubTheme {
-        Greeting("Android")
+        DeveloperInfo()
     }
 }
