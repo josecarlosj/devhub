70f74a37769e736658294447ee44182d3772c3d7
josecarlosj <junior.edf@gmail.com>
2023-07-12 08:53:02 -0300

    chore(day03): import Coil, loading profile picture from url and improve layout

--- app/src/main/java/com/jcjunior/devhub/MainActivity.kt
+++ app/src/main/java/com/jcjunior/devhub/MainActivity.kt
@@ -3,7 +3,6 @@ package com.jcjunior.devhub
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -22,12 +21,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.jcjunior.devhub.ui.theme.CustomBlue
 import com.jcjunior.devhub.ui.theme.DevHubTheme
 
@@ -49,7 +53,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DeveloperInfoScreen() {
-    val boxHeight = remember {
+    val defaultHeightComponents = remember {
         120.dp
     }
 
@@ -57,7 +61,7 @@ fun DeveloperInfoScreen() {
         Surface(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(boxHeight),
+                .height(defaultHeightComponents),
             shape = RoundedCornerShape(
                 bottomEndPercent = 25,
                 bottomStartPercent = 25
@@ -67,23 +71,34 @@ fun DeveloperInfoScreen() {
 
         Column(
             modifier = Modifier
-                .padding(top = boxHeight/2)
+                .padding(top = defaultHeightComponents/2)
                 .fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            Image(
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data("<profile picture>")
+                    .crossfade(true)
+                    .build(),
+                error = painterResource(R.drawable.avatar_sample),
+                placeholder = painterResource(R.drawable.avatar_sample),
+                contentDescription = stringResource(R.string.desc_profile_picture),
+                contentScale = ContentScale.Crop,
                 modifier = Modifier
-                    .size(boxHeight)
-                    .clip(CircleShape),
-                painter = painterResource(id = R.drawable.avatar_sample),
-                contentDescription = null,
+                    .size(defaultHeightComponents)
+                    .clip(CircleShape)
             )
+
             Column (horizontalAlignment = Alignment.CenterHorizontally) {
                 Text(text = "Fulano da Silva", fontSize = 24.sp)
                 Text(text = "fulanosilva-dev", fontWeight = FontWeight.Bold)
             }
-            Text(text = "Desenvolvedor apaixonado por tecnologia e programação.", fontSize = 14.sp)
+            Text(
+                modifier = Modifier.padding(horizontal = 18.dp),
+                textAlign = TextAlign.Center,
+                text = "Desenvolvedor apaixonado por tecnologia e programação."
+            )
         }
     }
 }
